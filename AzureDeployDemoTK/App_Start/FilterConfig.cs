b86f5a38a759486df1cd3c7931d83b63881084c9
karbe <karbe@EDS6673-MSDN>
2017-01-19 09:25:20 +0100

    Add project files.

---
+++ AzureDeployDemoTK/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AzureDeployDemoTK
+{
+  public class FilterConfig
+  {
+    public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+    {
+      filters.Add(new HandleErrorAttribute());
+    }
+  }
+}
---
+++ AzureDeployDemoTK/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AzureDeployDemoTK.MvcApplication" Language="C#" %>
