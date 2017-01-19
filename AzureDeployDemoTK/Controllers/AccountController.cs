5af8dfe0be0a36181cd01104a749bd1245c8ab36
karbe <karbe@EDS6673-MSDN>
2017-01-19 10:02:01 +0100

    TEST

--- AzureDeployDemoTK/Controllers/AccountController.cs
+++ AzureDeployDemoTK/Controllers/AccountController.cs
@@ -8,7 +8,7 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
-using AzureDeployDemoTK.Models;  
+using AzureDeployDemoTK.Models;     
 
 namespace AzureDeployDemoTK.Controllers
 {
